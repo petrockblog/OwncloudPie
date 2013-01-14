265b637bf52217d5d721b75313b09929c4d55e6b
Florian <petrockblock@gmail.com>
2013-01-14 06:50:22 -1000

    Update owncloudpie_setup.sh

--- owncloudpie_setup.sh
+++ owncloudpie_setup.sh
@@ -131,7 +131,7 @@ server {
 
   # enable php
   location ~ \.php$ {
-    try_files $uri =404;
+    try_files \$uri =404;
     fastcgi_pass 127.0.0.1:9000;
     fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
     fastcgi_param HTTPS on;
